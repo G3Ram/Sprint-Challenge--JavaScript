2baa301e97a8c9c60ec02822854e9096902607fa
Skyler Dowdy <skyler2440@gmail.com>
2019-04-05 13:45:51 -0400

    Array Update 2
    Having issues with for loops need to work on 3 problems.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -109,6 +109,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+let conferenceNames = zooAnimals.forEach(function(i){
+animalNames.push("Animal Name: " + i.animal_name + " " + "Scientific Name:" + " " +i.scientific_name);
+});
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -117,7 +120,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+let lowerCase = zooAnimals.map((animal) => {
+  return animal.animal_name.toLowerCase();
+})
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -125,7 +130,9 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+let largerPopulation = zooAnimals.filter((pop) => {
+return pop.population < 5;
+});
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -133,7 +140,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = zooAnimals.reduce((populationTotal, populationAmount) => {
+return populationTotal + populationAmount.population;
+}, 0);
 console.log(populationTotal);
 
 
