faf36212075e95f2e28a5c9146ac761af4a19e19
Andy Bettisworth <andy@accreu.com>
2019-04-05 12:28:01 -0500

    test: add assertions to classes.js

--- test/classes_test.js
+++ test/classes_test.js
@@ -18,6 +18,26 @@ const CubeMaker = classes.CubeMaker
  * Assertions
  */
 
-test.skip("Add tests!", t => {
-  t.fail()
+test("Volume of a cuboid length: 4, width: 5, height: 5 is 100", t => {
+  const cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 })
+
+  t.is(cuboid.volume(), 100)
+})
+
+test("Surface area of a cuboid length: 4, width: 5, height: 5 is 130", t => {
+  const cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 })
+
+  t.is(cuboid.surfaceArea(), 130)
+})
+
+test("Volume of a cube length: 4, width: 4, height: 4 is 64", t => {
+  const cube = new CubeMaker({ length: 4, width: 4, height: 4 })
+
+  t.is(cube.volume(), 64)
+})
+
+test("Surface area of a cube length: 4, width: 4, height: 4 is 96", t => {
+  const cube = new CubeMaker({ length: 4, width: 4, height: 4 })
+
+  t.is(cube.surfaceArea(), 96)
 })
