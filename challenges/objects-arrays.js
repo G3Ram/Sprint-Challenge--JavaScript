1109662775743c396e62a3f58854669459f10fdb
Andy Bettisworth <andy@accreu.com>
2019-04-05 11:04:26 -0500

    Complete tasks for college graduates

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,8 +76,15 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = []
+
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university)
+}
+universities.sort()
+
 console.log(universities)
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
 
@@ -85,12 +92,26 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+
+const contactInfo = []
+
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
+
 console.log(contactInfo)
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+
+const uni = []
+
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.match(/Uni/)) {
+    uni.push(graduates[i])
+  }
+}
+
 console.log(uni)
 
 
@@ -108,14 +129,14 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
 {"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
 {"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}]
 
 /* Request 1: .forEach()
 
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+const animalNames = []
 console.log(animalNames)
 
 /* Request 2: .map()
@@ -124,7 +145,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = []
 console.log(lowerCase)
 
 /* Request 3: .filter()
@@ -132,7 +153,7 @@ console.log(lowerCase)
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = []
 console.log(largerPopulation)
 
 /* Request 4: .reduce()
