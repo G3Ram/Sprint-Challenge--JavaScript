ea7be820289460b65e2f7536503eec346c106a3c
Andy Bettisworth <andy@accreu.com>
2019-04-05 12:22:40 -0500

    test: add assertions to functions.js

--- test/functions_test.js
+++ test/functions_test.js
@@ -20,6 +20,14 @@ const greeting = functions.greeting
  * Assertions
  */
 
-test.skip("Add tests!", t => {
-  t.fail()
+test("Does 2 + 2 == 4?", t => {
+  t.is(consume(2, 2, add), 4)
+})
+
+test("Does 10 * 16 == 160?", t => {
+  t.is(consume(10, 16, multiply), 160)
+})
+
+test("Greeting for Mary Poppins", t => {
+  t.is(consume("Mary", "Poppins", greeting), "Hello Mary Poppins, nice to meet you!")
 })
